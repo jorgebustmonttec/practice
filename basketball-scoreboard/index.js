b9aa2e0fb74812f10c83856d9880f53666c262f4
Jorge Bustamante <jemilianobustamante@gmail.com>
2022-12-13 18:32:25 -0600

    created basketball counter
    began html and css, simple js start

---
+++ basketball-scoreboard/index.js
@@ -0,0 +1,17 @@
+//let homePoints=document.getElementById()
+
+function test(team,points){
+    console.log("click")
+    let teamf=team;
+    let pointsf=points
+    if(team==0){
+        console.log("HOME")
+    }
+    if(team==1){
+        console.log("GUEST")
+    }
+
+    console.log(points)
+
+
+}
