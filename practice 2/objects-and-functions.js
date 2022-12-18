0520b640fd58d808dfbb3a9d096477bea7791e4b
Jorge Bustamante <jemilianobustamante@gmail.com>
2022-12-17 18:54:18 -0600

    practice 2 started

---
+++ practice 2/objects-and-functions.js
@@ -0,0 +1,10 @@
+person={
+    name: "Jorge",
+    age: 19,
+    country: "Mexico",
+}
+function logData() {
+    console.log(person.name + " is " + person.age + " years old and lives in " + person.country)
+}
+
+logData()
