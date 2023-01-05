0cc67ae303b6903d75923fbe9c9ae134ab2dc251
Jorge Bustamante <jemilianobustamante@gmail.com>
2023-01-04 19:38:45 -0700

    javascript progress
    started password gen function, all variables declared

--- password-generator/index.js
+++ password-generator/index.js
@@ -1,12 +1,27 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+const symbols =["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+
+const numbers =["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
 
 
 const symbolsCheck = document.querySelector('#symbolsOption')
 const numberCheck = document.querySelector('#numberOption')
 const lengthValue = document.querySelector('#passwordLength')
 
-function GeneratePassword(){
+function GeneratePassword(letters, symbols, numbers, symbolsCheck, numberCheck, lengthValue){
+    let characters = letters;
+
+    if (symbolsCheck.checked){
+        
+    }
+     
+}
+
+function GeneratePasswordFAKE(){
+    let passwordOne = document.getElementById("passwordOne");
     console.log("button press")
     if(symbolsCheck.checked){
         console.log("symbols checked")
@@ -21,6 +36,7 @@ function GeneratePassword(){
     }else{
         console.log(parseInt(lengthValue.value))
     }
+    passwordOne.textContent="hola"
 }
 
 
