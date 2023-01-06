ee04ccc9d61c702adf0ab1f1980415da46a15f85
Jorge Bustamante <jemilianobustamante@gmail.com>
2023-01-05 20:46:37 -0700

    password generator finished
    includes option to set length, and check numbers and/or symbols.

--- password-generator/index.js
+++ password-generator/index.js
@@ -1,5 +1,14 @@
 
 
+function checkValue(value, max, min){
+    if(value>max){
+        value=max
+    }else if(value<min){
+        value=min
+    }
+    return value;
+}
+
 function GeneratePassword(){
     const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
@@ -13,14 +22,34 @@ function GeneratePassword(){
     const numberCheck = document.querySelector('#numberOption')
     const lengthValue = document.querySelector('#passwordLength')
 
+    PasswordLength=checkValue(parseInt(lengthValue.value),parseInt(lengthValue.max),parseInt(lengthValue.min))
+
     if (symbolsCheck.checked){
         characters=characters.concat(symbols)
     }
     if(numberCheck.checked){
         characters=characters.concat(numbers)
     }
-    let randomIndex = Math.floor((Math.random()*characters.length)+1);
+    let randomIndex = Math.floor((Math.random()*characters.length));
 
     randomCharacter=characters[randomIndex]
-    console.log(randomCharacter)
+
+    let Password=""
+    for(let i=0;i<PasswordLength;i++){
+        let randomIndex = Math.floor((Math.random()*characters.length));
+
+        randomCharacter=characters[randomIndex];
+        Password=Password.concat(randomCharacter);
+    }
+    return Password
+}
+
+function AssignPassword(){
+    let passwordOneEl=document.querySelector('#passwordOne')
+    let passwordTwoEl=document.querySelector('#passwordTwo')
+    let Password=""
+    Password=GeneratePassword()
+    passwordOneEl.textContent=Password
+    Password=GeneratePassword()
+    passwordTwoEl.textContent=Password   
 }
