af29310aee2a53d78874ee3464292eb08e7c5387
Jorge Bustamante <jemilianobustamante@gmail.com>
2023-01-05 19:58:01 -0700

    updated password function
    generates random character properly

--- password-generator/index.js
+++ password-generator/index.js
@@ -1,42 +1,26 @@
-const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
-const symbols =["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
-
-const numbers =["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 
+function GeneratePassword(){
+    const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
-const symbolsCheck = document.querySelector('#symbolsOption')
-const numberCheck = document.querySelector('#numberOption')
-const lengthValue = document.querySelector('#passwordLength')
+    const symbols =["~","`","!","@","#","$","%","^","&","*","(",")","_", "-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
-function GeneratePassword(letters, symbols, numbers, symbolsCheck, numberCheck, lengthValue){
+    const numbers =["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    
     let characters = letters;
+    
+    const symbolsCheck = document.querySelector('#symbolsOption')
+    const numberCheck = document.querySelector('#numberOption')
+    const lengthValue = document.querySelector('#passwordLength')
 
     if (symbolsCheck.checked){
-        
-    }
-     
-}
-
-function GeneratePasswordFAKE(){
-    let passwordOne = document.getElementById("passwordOne");
-    console.log("button press")
-    if(symbolsCheck.checked){
-        console.log("symbols checked")
+        characters=characters.concat(symbols)
     }
     if(numberCheck.checked){
-        console.log("numbers checked")
+        characters=characters.concat(numbers)
     }
-    if(parseInt(lengthValue.value)>parseInt(lengthValue.max)){
-        console.log(parseInt(lengthValue.max))
-    } else if(parseInt(lengthValue.value)<parseInt(lengthValue.min)){
-        console.log(parseInt(lengthValue.min))
-    }else{
-        console.log(parseInt(lengthValue.value))
-    }
-    passwordOne.textContent="hola"
+    let randomIndex = Math.floor((Math.random()*characters.length)+1);
+
+    randomCharacter=characters[randomIndex]
+    console.log(randomCharacter)
 }
-
-
